d08b62644b5322cba0001e3d30652229213ab4cc
Tekinova Alina <alina.tekinova21@gmail.com>
2026-03-16 19:40:37 +0100

    [Api] created Artist, Album and Track mongoose models with relations

---
+++ api/models/Track.ts
@@ -0,0 +1,30 @@
+import mongoose, {Types} from "mongoose";
+import Album from "./Album.js";
+
+const Schema = mongoose.Schema;
+
+const TrackSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    album: {
+        type: Schema.Types.ObjectId,
+        ref: 'Album',
+        required: true,
+        validate: {
+        validator: async (value: Types.ObjectId) => {
+            const album = await Album.findById(value);
+            return Boolean(album);
+        },
+            message: 'Album does not exist!'
+        }
+    },
+    duration: {
+        type: String,
+        required: true
+    }
+});
+
+const Track = mongoose.model('Track', TrackSchema);
+export default Track;
