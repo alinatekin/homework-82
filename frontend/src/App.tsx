d1a87d42702909fe3d622f7ce3e04211e425be34
Tekinova Alina <alina.tekinova21@gmail.com>
2026-03-26 19:15:26 +0100

    [hw-85] setup frontend with MUI, Redux, and Router dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import {Container, Typography} from "@mui/material";
+
+const App = () => {
+
+  return (
+    <>
+      <Container>
+        <Typography variant="h3" sx={{ mt: 4 }}>
+          Music Catalog 🎵
+        </Typography>
+      </Container>
+    </>
+  )
+};
+
+export default App
